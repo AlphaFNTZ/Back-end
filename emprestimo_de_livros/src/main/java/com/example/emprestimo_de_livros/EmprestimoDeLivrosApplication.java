645fdcf3499fb62ef0baaef40d00823399a1266e
AlphaFNTZ <juliano.ramos@estudante.ufla.br>
2024-04-21 22:51:22 -0300

    Criando repositorio

---
+++ emprestimo_de_livros/src/main/java/com/example/emprestimo_de_livros/EmprestimoDeLivrosApplication.java
@@ -0,0 +1,13 @@
+package com.example.emprestimo_de_livros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprestimoDeLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprestimoDeLivrosApplication.class, args);
+	}
+
+}
---
+++ emprestimo_de_livros/src/test/java/com/example/emprestimo_de_livros/EmprestimoDeLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emprestimo_de_livros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprestimoDeLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
