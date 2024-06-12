8dd52ba92bf5d0e23f931a300d4cd70d8cc0bb6b
AlphaFNTZ <juliano.ramos@estudante.ufla.br>
2024-06-12 09:56:13 -0300

    Configurando o spring security

--- emprestimo_de_livros/src/main/java/com/example/emprestimo_de_livros/infra/configuracao/ConfigCors.java
+++ emprestimo_de_livros/src/main/java/com/example/emprestimo_de_livros/infra/configuracao/ConfigCors.java
@@ -11,7 +11,7 @@ public class ConfigCors implements WebMvcConfigurer
     public void addCorsMappings(CorsRegistry corsRegistry)
     {
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000","http://localhost:5173","http://localhost:5173/registro","http://192.168.1.103:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH")
                 .maxAge(3600)
                 .allowCredentials(true);
--- emprestimo_de_livros/src/main/java/com/example/emprestimo_de_livros/infra/configuracao/OpenApiConfig.java
+++ emprestimo_de_livros/src/main/java/com/example/emprestimo_de_livros/infra/configuracao/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package com.example.emprestimo_de_livros.infra.configuracao;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,8 +22,14 @@ public class OpenApiConfig {
                         .version("v1") //versao da documentacao
                         .description("Uma aplicação REST para o controle eficiente dos empréstimos de livros em bibliotecas. Essa ferramenta simplifica o registro e monitoramento dos livros emprestados, proporcionando uma visão organizada do movimento de informações essenciais para o funcionamento da biblioteca.") //descricao da documentacao
                 )
-                .servers(Arrays.asList(
-                        new Server().url("http://localhost:8080").description("Servidor local")
-                ));
+                .addServersItem(new Server().url("http://localhost:8080").description("Servidor local"))
+                .components(new Components()
+                        .addSecuritySchemes("bearer-jwt", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList("bearer-jwt", Arrays.asList("read", "write")));
     }
 }
